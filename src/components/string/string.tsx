ce0865ed4e73dde2687f7a6cb32d2a79d36eece5
web77code <web77@yandex.ru>
2022-10-04 16:30:36 +0300

    layout: add elements to StringComponent

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./string.module.css";
 
 export const StringComponent: React.FC = () => {
+
+  const [state, setState] = useState('');
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setState(evt.target.value);
+  }
+
+  const handleClick = () => {
+    setState('');
+  }
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.container}>
+        <Input 
+          type={"text"} 
+          style={{height: "60px"}} 
+          maxLength={11} 
+          isLimitText={true} 
+          value={state}
+          onChange={handleChange}
+        />
+        <Button text={"Развернуть"} onClick={handleClick} />
+      </div>
     </SolutionLayout>
   );
 };

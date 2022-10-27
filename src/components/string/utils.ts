016a2e5a1122f40d54f506128101bf341f4741f1
web77code <web77@yandex.ru>
2022-10-27 09:40:11 +0300

    fix: delete visualization part from swap function

--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -1,24 +1,11 @@
-import { ElementStates } from "../../types/element-states"
-import { TReverseStringResult } from "../../types/results";
+import { TReverseStringResult } from '../../types/results';
 
 export const swapElements = (array: TReverseStringResult, step: number) => {
   let res = [...array];
 
   let tmp = res[step];
   res[step] = res[res.length - step - 1];
-  res[res.length - step -1] = tmp;
+  res[res.length - step - 1] = tmp;
 
-  res[step].state = ElementStates.Modified;
-  res[res.length - step -1].state = ElementStates.Modified;
-
-  if(step + 1 < res.length - step - 2) {
-    res[step + 1].state = ElementStates.Changing;
-    res[res.length - step - 2].state = ElementStates.Changing;
-  }
-
-  if(step + 1 === res.length - step - 2) {
-    res[step + 1].state = ElementStates.Modified;
-  }
-  
   return res;
-}
+};

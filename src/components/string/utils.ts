2591ba8fd742db11c56c2bcd0b7d424dec54f63f
web77code <web77@yandex.ru>
2022-10-28 10:43:13 +0300

    refact: make ReverseRange class

--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -1,11 +1,52 @@
-import { TReverseStringResult } from '../../types/results';
+interface IReverseRange<T> {
+  _swap: () => void;
+  setState: (items: T[]) => void;
+  getState: () => T[];
+  nextStep: () => void;
+  startPosition: () => number;
+  endPosition: () => number;
+}
 
-export const swapElements = (array: TReverseStringResult, step: number) => {
-  let res = [...array];
+export class ReverseRange<T> implements IReverseRange<T> {
+  private _state: T[] = [];
+  private _start: number = 0;
+  private _end: number = 0;
 
-  let tmp = res[step];
-  res[step] = res[res.length - step - 1];
-  res[res.length - step - 1] = tmp;
+  isReversed: boolean = false;
 
-  return res;
-};
+  _swap() {
+    let tmp = this._state[this._start];
+    this._state[this._start] = this._state[this._end];
+    this._state[this._end] = tmp;
+  }
+
+  setState(items: T[]) {
+    if (items.length > 0) {
+      this._state = [...items];
+
+      this._start = 0;
+      this._end = items.length - 1;
+      this.isReversed = false;
+    }
+  }
+
+  getState() {
+    return this._state;
+  }
+
+  nextStep() {
+    if (!this.isReversed) {
+      this._swap();
+    }
+
+    this._start++;
+    this._end--;
+
+    if(this._start >= this._end) {
+      this.isReversed = true;
+    }
+  }
+
+  startPosition = () => this._start;
+  endPosition = () => this._end;
+}

6a6f4804e931a7e49c41592ed87955053b5214fa
web77code <web77@yandex.ru>
2022-12-13 10:19:46 +0300

    feat: add typing and digits validation to useFrom hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,20 +1,50 @@
 import { useState, ChangeEvent } from 'react';
 
-interface IInputValues {
-  [key: string]: string;
-}
+type TInputValues = {
+  [key: string]: {
+    value: string;
+    onlyDigits?: boolean;
+  };
+};
 
-export function useForm(inputValues: IInputValues) {
+type TUseForm<T> = {
+  values: T;
+  handleChange: (evt: ChangeEvent<HTMLInputElement>) => void;
+  clearValue: (name: string) => void;
+};
+
+export function useForm<T extends TInputValues>(inputValues: T): TUseForm<T> {
   const [values, setValues] = useState(inputValues);
 
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = evt.target;
-    setValues((prevState) => ({ ...prevState, [name]: value }));
+
+    if (values[name].onlyDigits && !value.match('^[0-9]*$')) {
+      return;
+    }
+
+    setValues((prevState) => ({
+      ...prevState,
+      [name]: {
+        value,
+        onlyDigits: prevState[`${name}`]?.onlyDigits,
+      },
+    }));
   };
 
   const clearValue = (name: string) => {
-    setValues((prevState) => ({ ...prevState, [name]: '' }));
+    setValues((prevState) => ({
+      ...prevState,
+      [name]: {
+        value: '',
+        onlyDigits: prevState[`${name}`]?.onlyDigits,
+      },
+    }));
   };
 
-  return { values, handleChange, setValues, clearValue };
+  return {
+    values,
+    handleChange,
+    clearValue,
+  };
 }
